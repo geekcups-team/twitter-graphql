3e85b23bb7b6d2c880361cbeaf6940acf75da663
Andrea Beggiato <andrea.beggiato@geekcups.com>
2017-10-03 14:41:10 +0200

    ecosystem

---
+++ server/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Twitter API',
+      script: 'server/app.js',
+      interpreter: 'node@8.5.0',
+      env: {
+        EXPRESS_PORT: 3010,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'deployer',
+      host: 'api-twitter.geekcups.com',
+      ref: 'origin/master',
+      repo: 'https://github.com/geekcups-team/twitter-graphql',
+      path: '/home/deployer/apps/production/twitter-graphq_server',
+      'post-deploy': '/home/deployer/.nvm/versions/node/v6.10.0/bin/npm install && /home/deployer/.nvm/versions/node/v6.10.0/lib/node_modules/pm2/bin/pm2 startOrRestart ecosystem.config.js --env staging',
+      env: {
+        NODE_ENV: 'production',
+        EXPRESS_PORT: 3010,
+      },
+    },
+  },
+};
