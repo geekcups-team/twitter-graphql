69709a7924269901af5822341538fca700869d14
Andrea Beggiato <andrea.beggiato@geekcups.com>
2017-10-03 14:46:14 +0200

    fix ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/geekcups-team/twitter-graphql',
       path: '/home/deployer/apps/production/twitter-graphq_server',
-      'post-deploy': '/home/deployer/.nvm/versions/node/v6.10.0/bin/npm install && /home/deployer/.nvm/versions/node/v6.10.0/lib/node_modules/pm2/bin/pm2 startOrRestart ecosystem.config.js --env staging',
+      'post-deploy': '/home/deployer/.nvm/versions/node/v6.10.0/bin/npm install && /home/deployer/.nvm/versions/node/v6.10.0/lib/node_modules/pm2/bin/pm2 startOrRestart server/ecosystem.config.js --env production',
       env: {
         NODE_ENV: 'production',
         EXPRESS_PORT: 3010,
