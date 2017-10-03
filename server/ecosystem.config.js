ca043dff96796bd3f276be47b633c62c1d384b85
Andrea Beggiato <andrea.beggiato@geekcups.com>
2017-10-03 15:04:18 +0200

    package.json

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/geekcups-team/twitter-graphql',
       path: '/home/deployer/apps/production/twitter-graphql_server',
-      'post-deploy': '/home/deployer/.nvm/versions/node/v6.10.0/bin/npm install server/package.json && /home/deployer/.nvm/versions/node/v6.10.0/lib/node_modules/pm2/bin/pm2 startOrRestart server/ecosystem.config.js --env production',
+      'post-deploy': '/home/deployer/.nvm/versions/node/v6.10.0/bin/npm --prefix /home/deployer/apps/production/twitter-graphql_server/current/server install && /home/deployer/.nvm/versions/node/v6.10.0/lib/node_modules/pm2/bin/pm2 startOrRestart server/ecosystem.config.js --env production',
       env: {
         NODE_ENV: 'production',
         EXPRESS_PORT: 3100,
